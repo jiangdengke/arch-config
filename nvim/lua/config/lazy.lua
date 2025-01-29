8d5bffb59b362066ea083a1f5a70863a008b1efd
jiangdk <jiang1728439852@gmail.com>
2025-01-29 23:01:19 +0800

    feat:update yazi nvim plugins

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 -- Make sure to setup `mapleader` and `maplocalleader` before
@@ -22,17 +22,20 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-    require("config.plugins.lualine"),
-    require("config.plugins.imgclip"),
-    require("config.plugins.markdown-preview"),
-    require("config.plugins.tokyonight"),
-    require("config.plugins.neo-tree"),
-    require("config.plugins.gitsigns"),
-    require("config.plugins.mini-pairs"),
-    require("config.plugins.ts-comments"),
-    -- require("config.plugins.lazydev"),
-    require("config.plugins.conform"),
-    require("config.plugins.nvim-treesitter"),
-    require("config.plugins.noice"),
-    require("config.plugins.bufferline"),
+	require("config.plugins.lualine"),
+	require("config.plugins.imgclip"),
+	require("config.plugins.markdown-preview"),
+	require("config.plugins.tokyonight"),
+	require("config.plugins.neo-tree"),
+	require("config.plugins.gitsigns"),
+	require("config.plugins.mini-pairs"),
+	require("config.plugins.ts-comments"),
+	-- require("config.plugins.lazydev"),
+	require("config.plugins.conform"),
+	require("config.plugins.nvim-treesitter"),
+	require("config.plugins.noice"),
+	require("config.plugins.bufferline"),
+	require("config.plugins.vim-easy-align"),
+	require("config.plugins.mason"),
+	require("config.plugins.nvim-lint"),
 })
